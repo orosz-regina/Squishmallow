5f96c5cfc47ae438b3c8589b59403921a69cbf53
orosz-regina <oroszr48@gmail.com>
2025-01-03 13:38:45 +0100

    Create Project

---
+++ src/main/java/squishmallow/SquishmallowApplication.java
@@ -0,0 +1,13 @@
+package squishmallow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquishmallowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquishmallowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/squishmallow/SquishmallowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.squishmallow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquishmallowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
