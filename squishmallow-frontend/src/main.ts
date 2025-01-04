d01812dfdd2e2014a1269dd63fc93cdef91c47ac
orosz-regina <oroszr48@gmail.com>
2025-01-04 21:52:08 +0100

    Squish list

--- squishmallow-frontend/src/main.ts
+++ squishmallow-frontend/src/main.ts
@@ -1,6 +1,11 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule)  // Itt adod hozzá az HttpClientModule-t
+  ]
+})
+  .catch(err => console.error(err));
